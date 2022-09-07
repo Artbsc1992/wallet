2d6c3efe9662b1b7fc83805e962fda57d2e44653
arturo <ahpareja@gmail.com>
2022-09-07 17:22:58 -0500

    add  operations and group_operation to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,18 +24,18 @@ group1_2 = Group.create(name: 'Travel', icon: 'https://png.pngtree.com/png-clipa
 group2_1 = Group.create(name: 'Rent', icon: 'https://thumbs.dreamstime.com/z/house-rent-vector-illustration-52179738.jpg', user_id: user2.id)
 group2_2 = Group.create(name: 'Pets', icon: 'https://www.goredforwomen.org/-/media/Healthy-Living-Images/Healthy-Lifestyle/Pets/puppy-kitten-heart.jpg', user_id: user2.id)
 
-# Operations
-# op1_1 = Operation.create(name: 'Pizza', amount: 15.50, author_id: user1.id)
-# op1_2 = Operation.create(name: 'Tuna Sandwich', amount: 3.15, author_id: user1.id)
-# op1_3 = Operation.create(name: 'Electricity', amount: 190.50, author_id: user1.id)
-# op1_4 = Operation.create(name: 'Gas', amount: 301.98, author_id: user1.id)
-# op2_1 = Operation.create(name: 'House A', amount: 250700.00, author_id: user2.id)
-# op2_2 = Operation.create(name: 'House B', amount: 390500.00, author_id: user2.id)
-# op2_3 = Operation.create(name: 'John', amount: 312.00, author_id: user2.id)
-# op2_4 = Operation.create(name: 'Mill', amount: 312.00, author_id: user2.id)
+#Operations
+op1_1 = Operation.create(name: 'Pizza', amount: 15.50, user_id: user1.id)
+op1_2 = Operation.create(name: 'Tuna Sandwich', amount: 3.15, user_id: user1.id)
+op1_3 = Operation.create(name: 'Electricity', amount: 190.50, user_id: user1.id)
+op1_4 = Operation.create(name: 'Gas', amount: 301.98, user_id: user1.id)
+op2_1 = Operation.create(name: 'House A', amount: 250700.00, user_id: user2.id)
+op2_2 = Operation.create(name: 'House B', amount: 390500.00, user_id: user2.id)
+op2_3 = Operation.create(name: 'John', amount: 312.00, user_id: user2.id)
+op2_4 = Operation.create(name: 'Mill', amount: 312.00, user_id: user2.id)
 
-# # Group Operations
-# group1_1.operations += [op1_1, op1_2]
-# group1_2.operations += [op1_3, op1_4]
-# group2_1.operations += [op2_1, op2_2]
-# group2_2.operations += [op2_3, op2_4]
+# Group Operations
+group1_1.operations += [op1_1, op1_2]
+group1_2.operations += [op1_3, op1_4]
+group2_1.operations += [op2_1, op2_2]
+group2_2.operations += [op2_3, op2_4]
