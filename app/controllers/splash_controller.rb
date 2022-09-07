6e4f57a66970f7049a0424064f7cfe3570789668
arturo <ahpareja@gmail.com>
2022-09-07 11:18:11 -0500

    linters fix

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,8 +1,6 @@
 class SplashController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    if user_signed_in?
-      redirect_to groups_path
-    end
+    redirect_to groups_path if user_signed_in?
   end
 end
