6e8710a8fc2a69c6075dbf107d20273d4910410d
arturo <ahpareja@gmail.com>
2022-09-09 12:06:41 -0500

    Linters fixed

--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -17,4 +17,3 @@ RSpec.describe Operation, type: :model do
     end
   end
 end
-
--- spec/requests/splash_spec.rb
+++ spec/requests/splash_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Splashes', type: :request do
-
   describe 'GET /index' do
     before :each do
       get root_path
     end
 
-      it 'renders the index template' do
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
 
-        expect(response).to render_template(:index)
-      end
-
-      it 'returns http success' do
-
-        expect(response).to have_http_status(:success)
-      end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
   end
 end
